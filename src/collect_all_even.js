bf4cd2908915cb921dfa3c919dca2c98de3ab95f
jojo153377 <774948439@qq.com>
2016-05-26 22:46:33 +0800

    collect提交

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,12 @@
 
 function collect_same_elements(collection_a, object_b) {
- //在此处写代码
+	var mychar = [];
+ 	for(var i=0;i<collection_a.length;i++){
+		var index = object_b.value.indexOf(collection_a[i].key);
+			if(index>=0){
+				mychar.push(collection_a[i].key);
+			}
+	}
+	return mychar;
 }
-
 module.exports = collect_same_elements;
